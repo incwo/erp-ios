5bd4f1712867bfc39e034463f99d4d5d1fd7686a
Renaud Pradenc <rpradenc@incwo.com>
2019-08-27 17:36:06 +0200

    [ref] NewsVC: extracted XML parsing to a new method.

--- Classes/FCLNewsViewController.m
+++ Classes/FCLNewsViewController.m
@@ -98,74 +98,13 @@
     if (!data) return;
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-        
-        NSMutableArray* newsItems = [NSMutableArray array];
-        
-        __block NSMutableDictionary* item = nil;
-        
-        OAXMLDecoder* xmlDecoder = [OAXMLDecoder parseData:data withBlock:^(OAXMLDecoder *decoder) {
-            
-            //<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:trackback="http://madskills.com/public/xml/rss/module/trackback/">
-            //    <channel>
-            //        <title>le blog incwo.com</title>
-            //        <link>http://blog.incwo.com</link>
-            //        <language>en-us</language>
-            //        <ttl>40</ttl>
-            //        <description></description>
-            //        <item>
-            //            <title>Recevez des documents dans vos emails de rappels de t&#226;che et de rendez-vous</title>
-            //            <description>&lt;p&gt;&lt;strong&gt;Une nouvelle fonctionnalit&amp;eacute;&lt;/strong&gt; est disponible imm&amp;eacute;diatement sur les applications de gestion &lt;a href="http://www.incwo.com/" target="_blank"&gt;bureau virtuel&lt;/a&gt;, &lt;a href="http://www.gestion-de-chantier.com/" target="_blank"&gt;gestion-de-chantier&lt;/a&gt;, &lt;a href="http://www.ma-facturation.com" target="_blank"&gt;ma-facturation.com&lt;/a&gt; et &lt;a href="http://www.gestion-auto-entrepreneur.com" target="_blank"&gt;gestion-auto-entrepreneur.com&lt;/a&gt;. &lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;img src="http://www.incwo.com/images/blog/email_attached_files.jpg" border="0" alt="" width="331" height="110" align="right" /&gt;&lt;/strong&gt;D&amp;eacute;sormais, les emails de rappel de t&amp;acirc;ches ou d&amp;#39;&amp;eacute;v&amp;eacute;nements que vous recevez contiennent un lien vers les fichiers qui leur sont attach&amp;eacute;s. Acc&amp;eacute;dez ainsi directement depuis votre email aux documents n&amp;eacute;cessaires pour r&amp;eacute;aliser une t&amp;acirc;che, pr&amp;eacute;parer &amp;agrave; un rendez-vous, etc.&amp;nbsp; &lt;/p&gt;&lt;p&gt;&lt;em&gt;Cette nouvelle fonctionnalit&amp;eacute; est &lt;strong&gt;disponible d&amp;egrave;s  maintenant&lt;/strong&gt;,  pour l&amp;#39;essayer, &lt;a href="https://www.incwo.com/site/compte" target="_blank"&gt;connectez-vous   sur votre application&lt;/a&gt;.&amp;nbsp; &lt;/em&gt;&lt;/p&gt;</description>
-            //            <pubDate>Thu, 31 Jan 2013 14:40:00 +0100</pubDate>
-            //            <guid isPermaLink="false">urn:uuid:839186df-9948-4c67-a548-f86b50832375</guid>
-            //            <author>incwo</author>
-            //            <link>http://blog.incwo.com/articles/2013/01/31/recevez-des-documents-dans-vos-emails-de-rappels-de-t%C3%A2che-et-de-rendez-vous</link>
-            //            <category>Nos applications</category>
-            //            <category>taches</category>
-            //            <category>evenements</category>
-            //            <category>documents</category>
-            //        </item>
-            
-            [decoder startElement:@"rss" withBlock:^{
-                [decoder startElement:@"channel" withBlock:^{
-                    [decoder startElement:@"item" withBlock:^{
-                    
-                        item = [NSMutableDictionary dictionary];
-                        
-                        [decoder endElement:@"title" withBlock:^{
-                            item[@"title"] = decoder.currentStringStripped ?: @"";
-                        }];
-                        [decoder endElement:@"description" withBlock:^{
-                            item[@"HTML"] = decoder.currentStringStripped ?: @"";
-                        }];
-                        [decoder endElement:@"pubDate" withBlock:^{
-                            NSDate* date = [NSDate dateWithHTTPDate:decoder.currentStringStripped ?: @""];
-                            if (date) item[@"date"] = date;
-                        }];
-                        [decoder endElement:@"guid" withBlock:^{
-                            item[@"UUID"] = decoder.currentStringStripped ?: @"";
-                        }];
-                        [decoder endElement:@"link" withBlock:^{
-                            // TODO: parse the created_at into a NSDate object
-                            NSURL* url = [NSURL URLWithString:decoder.currentStringStripped];
-                            if (url) item[@"URL"] = url;
-                        }];
-                    }];
-                    [decoder endElement:@"item" withBlock:^{
-                        if (item.count > 0) [newsItems addObject:item];
-                    }];
-                }];
-            }];
-        }];
-        
-        if (xmlDecoder.error)
-        {
-            NSLog(@"News RSS parsing error: %@", xmlDecoder.error);
-        }
-        //NSLog(@"Parsed %d video items: %@", videoItems.count, videoItems);
-        
+        NSError *error;
+        NSArray *newsItems = [self newItemsFromXMLData:data error:&error];
+
         dispatch_async(dispatch_get_main_queue(), ^{
-            if (xmlDecoder.error) {
-                [self FCL_presentAlertForError:xmlDecoder.error];
+            if (!newsItems) {
+                NSLog(@"News RSS parsing error: %@", error);
+                [self FCL_presentAlertForError:error];
             }
             
             if (newsItems.count > 0)
@@ -179,7 +118,70 @@
     });
 }
 
+-(NSArray <NSDictionary *> *)newItemsFromXMLData:(NSData *)data error:(NSError **)outError {
+    NSMutableArray* newsItems = [NSMutableArray array];
+    __block NSMutableDictionary* item = nil;
+    OAXMLDecoder* xmlDecoder = [OAXMLDecoder parseData:data withBlock:^(OAXMLDecoder *decoder) {
+        
+        [decoder startElement:@"rss" withBlock:^{
+            [decoder startElement:@"channel" withBlock:^{
+                [decoder startElement:@"item" withBlock:^{
+                    
+                    item = [NSMutableDictionary dictionary];
+                    
+                    [decoder endElement:@"title" withBlock:^{
+                        item[@"title"] = decoder.currentStringStripped ?: @"";
+                    }];
+                    [decoder endElement:@"description" withBlock:^{
+                        item[@"HTML"] = decoder.currentStringStripped ?: @"";
+                    }];
+                    [decoder endElement:@"pubDate" withBlock:^{
+                        NSDate* date = [NSDate dateWithHTTPDate:decoder.currentStringStripped ?: @""];
+                        if (date) item[@"date"] = date;
+                    }];
+                    [decoder endElement:@"guid" withBlock:^{
+                        item[@"UUID"] = decoder.currentStringStripped ?: @"";
+                    }];
+                    [decoder endElement:@"link" withBlock:^{
+                        // TODO: parse the created_at into a NSDate object
+                        NSURL* url = [NSURL URLWithString:decoder.currentStringStripped];
+                        if (url) item[@"URL"] = url;
+                    }];
+                }];
+                [decoder endElement:@"item" withBlock:^{
+                    if (item.count > 0) [newsItems addObject:item];
+                }];
+            }];
+        }];
+    }];
+    
+    if (xmlDecoder.error) {
+        *outError = xmlDecoder.error;
+        return nil;
+    }
+    
+    return newsItems;
+}
 
+//<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:trackback="http://madskills.com/public/xml/rss/module/trackback/">
+//    <channel>
+//        <title>le blog incwo.com</title>
+//        <link>http://blog.incwo.com</link>
+//        <language>en-us</language>
+//        <ttl>40</ttl>
+//        <description></description>
+//        <item>
+//            <title>Recevez des documents dans vos emails de rappels de t&#226;che et de rendez-vous</title>
+//            <description>&lt;p&gt;&lt;strong&gt;Une nouvelle fonctionnalit&amp;eacute;&lt;/strong&gt; est disponible imm&amp;eacute;diatement sur les applications de gestion &lt;a href="http://www.incwo.com/" target="_blank"&gt;bureau virtuel&lt;/a&gt;, &lt;a href="http://www.gestion-de-chantier.com/" target="_blank"&gt;gestion-de-chantier&lt;/a&gt;, &lt;a href="http://www.ma-facturation.com" target="_blank"&gt;ma-facturation.com&lt;/a&gt; et &lt;a href="http://www.gestion-auto-entrepreneur.com" target="_blank"&gt;gestion-auto-entrepreneur.com&lt;/a&gt;. &lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;img src="http://www.incwo.com/images/blog/email_attached_files.jpg" border="0" alt="" width="331" height="110" align="right" /&gt;&lt;/strong&gt;D&amp;eacute;sormais, les emails de rappel de t&amp;acirc;ches ou d&amp;#39;&amp;eacute;v&amp;eacute;nements que vous recevez contiennent un lien vers les fichiers qui leur sont attach&amp;eacute;s. Acc&amp;eacute;dez ainsi directement depuis votre email aux documents n&amp;eacute;cessaires pour r&amp;eacute;aliser une t&amp;acirc;che, pr&amp;eacute;parer &amp;agrave; un rendez-vous, etc.&amp;nbsp; &lt;/p&gt;&lt;p&gt;&lt;em&gt;Cette nouvelle fonctionnalit&amp;eacute; est &lt;strong&gt;disponible d&amp;egrave;s  maintenant&lt;/strong&gt;,  pour l&amp;#39;essayer, &lt;a href="https://www.incwo.com/site/compte" target="_blank"&gt;connectez-vous   sur votre application&lt;/a&gt;.&amp;nbsp; &lt;/em&gt;&lt;/p&gt;</description>
+//            <pubDate>Thu, 31 Jan 2013 14:40:00 +0100</pubDate>
+//            <guid isPermaLink="false">urn:uuid:839186df-9948-4c67-a548-f86b50832375</guid>
+//            <author>incwo</author>
+//            <link>http://blog.incwo.com/articles/2013/01/31/recevez-des-documents-dans-vos-emails-de-rappels-de-t%C3%A2che-et-de-rendez-vous</link>
+//            <category>Nos applications</category>
+//            <category>taches</category>
+//            <category>evenements</category>
+//            <category>documents</category>
+//        </item>
 
 
 #pragma mark - Read Count
