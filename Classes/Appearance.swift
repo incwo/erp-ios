255de303781b4f8b16a27548124f090909ea82d2
Renaud Pradenc <rpradenc@incwo.com>
2019-10-28 12:05:16 +0100

    Colors are declared in an Asset catalogue so they can be switched from Light mode to Dark mode automatically. The old mechanism is conserved on iOS < 11 to ensure compatibility.

--- Classes/Appearance.swift
+++ Classes/Appearance.swift
@@ -11,27 +11,35 @@ import UIKit
 class Appearance: NSObject {
     
     @objc static func setup() {
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: darkGrey()]
-        UINavigationBar.appearance().tintColor = blue()
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: navigationBarTitleColor()]
+        UINavigationBar.appearance().tintColor = accentColor()
         
-        UITabBar.appearance().tintColor = blue()
+        UITabBar.appearance().tintColor = accentColor()
     }
     
     // MARK: Colors
     
-    @objc static func lightGrey() -> UIColor {
-        UIColor(white: 222.0/255.0, alpha: 1.0)
+    private static func navigationBarTitleColor() -> UIColor {
+        if #available(iOS 11, *) {
+            return UIColor(named: "navigationBar.title")!
+        } else {
+            return UIColor(white: 65.0/255.0, alpha: 1.0)
+        }
     }
     
-    @objc static func darkGrey() -> UIColor {
-        UIColor(white: 65.0/255.0, alpha: 1.0)
+    @objc static func accentColor() -> UIColor {
+        if #available(iOS 11, *) {
+            return UIColor(named: "accent")!
+        } else {
+            return UIColor(red: 20.0/255.0, green: 93.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+        }
     }
     
-    @objc static func blue() -> UIColor {
-        UIColor(red: 20.0/255.0, green: 93.0/255.0, blue: 151.0/255.0, alpha: 1.0)
-    }
-    
-    @objc static func red() -> UIColor {
-        UIColor(red: 218.0/255.0, green: 79.0/255.0, blue: 73.0/255.0, alpha: 1.0)
+    @objc static func destructiveActionColor() -> UIColor {
+        if #available(iOS 11, *) {
+            return UIColor(named: "destructiveAction")!
+        } else {
+            return UIColor(red: 218.0/255.0, green: 79.0/255.0, blue: 73.0/255.0, alpha: 1.0)
+        }
     }
 }
